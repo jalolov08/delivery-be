44ea2af024cf956cc96f1395411b7f14d8938cfa
HP\HP <aleksandr.asad7@gmail.com>
2024-12-10 20:07:09 +0500

    new chages

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import mongoose from "mongoose";
-import express from "express"
+import express from "express";
 import { mongodbUri, port } from "./config";
 import { router } from "./routes/routes";
-
+import cors from "cors"
 mongoose
-    .connect(mongodbUri, {})
-    .then(() => {
-        console.log("Успешно подключено к MongoDB");
-    })
-    .catch((error) => {
-        console.error("Ошибка при подключение к MongoDB:", error);
-    });
+  .connect(mongodbUri, {})
+  .then(() => {
+    console.log("Успешно подключено к MongoDB");
+  })
+  .catch((error) => {
+    console.error("Ошибка при подключение к MongoDB:", error);
+  });
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/uploads", express.static("uploads"));
-app.use("/api", router)
+app.use("/api", router);
 
 app.listen(port, () => {
-    console.log(`Сервер запущен на порту ${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { changeProfile, getMe } from "../controllers/user.controller";
+import {
+  changeProfile,
+  createAddress,
+  deleteAddress,
+  getMe,
+  updateAddress,
+} from "../controllers/user.controller";
 import checkAuth from "../utils/checkAuth";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
@@ -8,17 +14,20 @@ import path from "path";
 export const userRouter: Router = Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/users");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = uuidv4();
-        const ext = path.extname(file.originalname);
-        cb(null, file.fieldname + "-" + uniqueSuffix + ext);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "uploads/users");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = uuidv4();
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
 });
 
 const upload = multer({ storage: storage });
 
-userRouter.get("/me", checkAuth, getMe)
-userRouter.put("/profile", checkAuth, upload.single("photo"), changeProfile)
+userRouter.get("/me", checkAuth, getMe);
+userRouter.put("/profile", checkAuth, upload.single("photo"), changeProfile);
+userRouter.post("/address", checkAuth, createAddress);
+userRouter.delete("/address/:id", checkAuth, deleteAddress);
+userRouter.put("/address/:id", checkAuth, updateAddress);
